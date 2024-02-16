c8214c5ef029da5f59279e105f635dc23786a6cb
sghda <salmaghaida@upi.edu>
2024-02-16 14:48:19 +0700

    tadi di main masih akses selain cucu

--- Java/Program/Main.java
+++ Java/Program/Main.java
@@ -3,21 +3,21 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<Product> products = new ArrayList<>();
+        ArrayList<Shirt> shirt_list = new ArrayList<>(); // Menggunakan ArrayList<Shirt>
         Scanner scanner = new Scanner(System.in);
         int more_input = 1;
 
         // menambahkan data produk
-        products.add(new Shirt("Kemeja", "Gucci", 500000, "M", "Katun", "Pria", "Putih", "Pendek"));
-        products.add(new Shirt("Kemeja Lucu", "Gucci", 800000, "M", "Flanel", "Pria", "Pink", "Panjang"));
+        shirt_list.add(new Shirt("Kemeja", "Gucci", 500000, "M", "Katun", "Pria", "Putih", "Pendek"));
+        shirt_list.add(new Shirt("Kemeja Lucu", "Gucci", 800000, "M", "Flanel", "Pria", "Pink", "Panjang"));
 
         // Menampilkan data awal
         System.out.println("Daftar Produk:");
-        displayProducts(products);
+        displayShirt_list(shirt_list); // Memanggil metode displayShirt_list
 
         // Menambahkan data baru dari input user
         while (more_input == 1) {
-            System.out.println("\nTambah Data Produk Baru dengan id " + (products.size() + 1));
+            System.out.println("\nTambah Data Produk Baru dengan id " + (shirt_list.size() + 1));
             System.out.print("Nama: ");
             String name = scanner.nextLine();
             System.out.print("Merek: ");
@@ -38,11 +38,11 @@ public class Main {
 
             // Membuat objek baru dan menambahkannya ke daftar produk
             Shirt newShirt = new Shirt(name, brand, price, size, material, gender, color, sleeveType);
-            products.add(newShirt);
+            shirt_list.add(newShirt);
 
             // Menampilkan data setelah penambahan
             System.out.println("\nDaftar Produk Setelah Penambahan:");
-            displayProducts(products);
+            displayShirt_list(shirt_list); // Memanggil metode displayShirt_list
 
             System.out.println("\nApakah Anda ingin menambahkan data produk lagi? (1 = Ya, 0 = Tidak)");
             more_input = scanner.nextInt();
@@ -54,7 +54,7 @@ public class Main {
     }
 
     // Metode untuk menampilkan data produk
-    private static void displayProducts(ArrayList<Product> products) {
+    private static void displayShirt_list(ArrayList<Shirt> shirt_list) {
         int maxIdLength = 2;
         int maxNameLength = 4;
         int maxBrandLength = 5;
@@ -64,41 +64,38 @@ public class Main {
         int maxGenderLength = 6;
         int maxColorLength = 5;
         int maxSleeveTypeLength = 11;
-        
-        for (Product product : products) {
-            if (product instanceof Shirt) {
-                Shirt shirt = (Shirt) product;
-                if (shirt.getIdProduct() > maxIdLength) {
-                    maxIdLength = shirt.getIdProduct();
-                }
-                if (shirt.getName().length() > maxNameLength) {
-                    maxNameLength = shirt.getName().length();
-                }
-                if (shirt.getBrand().length() > maxBrandLength) {
-                    maxBrandLength = shirt.getBrand().length();
-                }
-                if (String.valueOf(shirt.getPrice()).length() > maxPriceLength) {
-                    maxPriceLength = String.valueOf(shirt.getPrice()).length();
-                }
-                if (shirt.getSize().length() > maxSizeLength) {
-                    maxSizeLength = shirt.getSize().length();
-                }
-                if (shirt.getMaterial().length() > maxMaterialLength) {
-                    maxMaterialLength = shirt.getMaterial().length();
-                }
-                if (shirt.getGender().length() > maxGenderLength) {
-                    maxGenderLength = shirt.getGender().length();
-                }
-                if (shirt.getColor().length() > maxColorLength) {
-                    maxColorLength = shirt.getColor().length();
-                }
-                if (shirt.getSleeveType().length() > maxSleeveTypeLength) {
-                    maxSleeveTypeLength = shirt.getSleeveType().length();
-                }
+
+        for (Shirt shirt : shirt_list) {
+            if (shirt.getIdProduct() > maxIdLength) {
+                maxIdLength = shirt.getIdProduct();
+            }
+            if (shirt.getName().length() > maxNameLength) {
+                maxNameLength = shirt.getName().length();
+            }
+            if (shirt.getBrand().length() > maxBrandLength) {
+                maxBrandLength = shirt.getBrand().length();
+            }
+            if (String.valueOf(shirt.getPrice()).length() > maxPriceLength) {
+                maxPriceLength = String.valueOf(shirt.getPrice()).length();
+            }
+            if (shirt.getSize().length() > maxSizeLength) {
+                maxSizeLength = shirt.getSize().length();
+            }
+            if (shirt.getMaterial().length() > maxMaterialLength) {
+                maxMaterialLength = shirt.getMaterial().length();
+            }
+            if (shirt.getGender().length() > maxGenderLength) {
+                maxGenderLength = shirt.getGender().length();
+            }
+            if (shirt.getColor().length() > maxColorLength) {
+                maxColorLength = shirt.getColor().length();
+            }
+            if (shirt.getSleeveType().length() > maxSleeveTypeLength) {
+                maxSleeveTypeLength = shirt.getSleeveType().length();
             }
         }
 
-
+        // Tampilkan header tabel
         System.out.print("+");
         for (int i = 0; i < maxIdLength + 2; i++) {
             System.out.print("-");
@@ -137,8 +134,10 @@ public class Main {
         }
         System.out.println("+");
 
+        // Tampilkan baris header
         System.out.printf("| %-" + maxIdLength + "s | %-" + maxNameLength + "s | %-" + maxBrandLength + "s | %-" + maxPriceLength + "s | %-" + maxSizeLength + "s | %-" + maxMaterialLength + "s | %-" + maxGenderLength + "s | %-" + maxColorLength + "s | %-" + maxSleeveTypeLength + "s |\n", "ID", "Nama", "Brand", "Harga", "Size", "Material", "Gender", "Warna", "Sleeve Type");
         
+        // Tampilkan garis pemisah antara header dan data
         System.out.print("+");
         for (int i = 0; i < maxIdLength + 2; i++) {
             System.out.print("-");
@@ -177,14 +176,13 @@ public class Main {
         }
         System.out.println("+");
 
-        for (Product product : products) {
-            if (product instanceof Shirt) {
-                Shirt shirt = (Shirt) product;
-                System.out.printf("| %-" + maxIdLength + "d | %-" + maxNameLength + "s | %-" + maxBrandLength + "s | %-" + maxPriceLength + "d | %-" + maxSizeLength + "s | %-" + maxMaterialLength + "s | %-" + maxGenderLength + "s | %-" + maxColorLength + "s | %-" + maxSleeveTypeLength + "s |\n",
-                        shirt.getIdProduct(), shirt.getName(), shirt.getBrand(), shirt.getPrice(), shirt.getSize(), shirt.getMaterial(), shirt.getGender(), shirt.getColor(), shirt.getSleeveType());
-            }
+        // Tampilkan data produk
+        for (Shirt shirt : shirt_list) {
+            System.out.printf("| %-" + maxIdLength + "d | %-" + maxNameLength + "s | %-" + maxBrandLength + "s | %-" + maxPriceLength + "d | %-" + maxSizeLength + "s | %-" + maxMaterialLength + "s | %-" + maxGenderLength + "s | %-" + maxColorLength + "s | %-" + maxSleeveTypeLength + "s |\n",
+                    shirt.getIdProduct(), shirt.getName(), shirt.getBrand(), shirt.getPrice(), shirt.getSize(), shirt.getMaterial(), shirt.getGender(), shirt.getColor(), shirt.getSleeveType());
         }
 
+        // Tampilkan garis penutup
         System.out.print("+");
         for (int i = 0; i < maxIdLength + 2; i++) {
             System.out.print("-");
